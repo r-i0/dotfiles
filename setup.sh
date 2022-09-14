1a44ff39b2a69469c4c165e3ec7bfcf3859c63e8
rsudo <badbad7351@gmail.com>
2022-09-14 21:28:45 +0900

    replace

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#
+install_brew () {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  brew install google-chrome \
+    firefox \
+    slack \
+    notion \
+    visual-studio-code \
+    bat \
+    ripgrep \
+    go \
+    jq \
+    exa \
+    fzf \
+    z \
+    gh \
+    neovim \
+    node \
+    yarn \
+    koekeishiya/formulae/yabai \
+    koekeishiya/formulae/skhd \
+
+  brew install --cask docker
+  brew install --cask wez/wezterm/wezterm
+}
+
+ln -is ${PWD}/zsh/zshrc ~/.zshrc
+ln -is ${PWD}/yabai/yabairc ~/.yabairc
+ln -is ${PWD}/yabai/skhdrc ~/.skhdrc
+
+install
