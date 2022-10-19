aa6e1529b1ead44e7bf7fae57d94f9ba6725c224
riosudo <badbad7351@gmail.com>
2022-10-19 18:35:22 +0900

    add git alias

--- zsh/zsh/alias.zsh
+++ zsh/zsh/alias.zsh
@@ -1,5 +1,9 @@
 alias c="clear"
 alias ls="exa --icons"
-alias gitlog='git log --all --graph --pretty=format:"%Cred%h%Creset %C(bold blue)%an%Creset :%C(yellow)%d%Creset %s %Cgreen(%cr) %Creset" --abbrev-commit'
 alias pb="pbcopy"
 alias t="translate"
+
+# git
+alias gitlog='git log --all --graph --pretty=format:"%Cred%h%Creset %C(bold blue)%an%Creset :%C(yellow)%d%Creset %s %Cgreen(%cr) %Creset" --abbrev-commit'
+alias gsb='git status -sb'
+alias lg='lazygit'
