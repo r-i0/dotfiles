edbe003449812e3e653271d26339296f34517c94
riosudo <badbad7351@gmail.com>
2023-01-11 08:41:34 +0900

    fix prompt

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,80 +1,83 @@
-function left-prompt {
-  name_t='179m%}'      # user name text clolr
-  name_b='000m%}'    # user name background color
-  path_t='255m%}'     # path text clolr
-  path_b='031m%}'   # path background color
-  arrow='087m%}'   # arrow color
-  text_color='%{\e[38;5;'    # set text color
-  back_color='%{\e[30;48;5;' # set background color
-  reset='%{\e[0m%}'   # reset
-  sharp='\uE0B0'      # triangle
+function prompt {
+  brace_start='%{'
+  brace_end='%}'
 
-  dir="${back_color}${path_b}${text_color}${path_t}"
-  echo "${back_color}${path_b}${text_color}${name_b}${sharp} ${dir}%~${reset}${text_color}${path_b}${sharp}${reset}\n${text_color}${arrow}→ ${reset}"
+  back_light_blue='\e[30;48;5;031m'
+
+  text_white='\e[38;5;255m'
+  text_light_blue='\e[38;5;031m'
+  text_cyan='\e[38;5;087m'
+
+  reset='%{\e[0m%}'
+
+  triangle='\uE0B0'
+  allow='\uf101'
+
+  dir_back_color="${brace_start}${back_light_blue}${brace_end}"
+  dir_text_color="${brace_start}${text_white}${brace_end}"
+  triangle_color="${brace_start}${text_light_blue}${brace_end}"
+  dir="${dir_back_color}${triangle}${dir_text_color} %~${reset}${triangle_color}${triangle}${reset}"
+
+  allow_color="${brace_start}${text_cyan}${brace_end}"
+  allow_with_color="${allow_color}${allow}${reset}"
+
+  echo "${dir} \n${allow_with_color} "
 }
 
-PROMPT=`left-prompt`
-
-# コマンドの実行ごとに改行
-function precmd() {
-    # Print a newline before the prompt, unless it's the
-    # first prompt in the process.
-    if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
-        NEW_LINE_BEFORE_PROMPT=1
-    elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
-        echo ""
-    fi
-}
-
-# git ブランチ名を色付きで表示させるメソッド
-function rprompt-git-current-branch {
-  local branch_name st branch_status
-
+# git ブランチ名を色付きで表示させる
+function git-current-branch {
   branch='\ue0a0'
-  color='%{\e[38;5;' #  文字色を設定
-  green='114m%}'
-  red='001m%}'
-  yellow='227m%}'
-  blue='033m%}'
-  reset='%{\e[0m%}'   # reset
-
-  color='%{\e[38;5;' #  文字色を設定
-  green='114m%}'
+  green='%{\e[38;5;114m%}'
+  red='%{\e[38;5;001m%}'
+  yellow='%{\e[38;5;227m%}'
+  blue='%{\e[38;5;033m%}'
+  reset='%{\e[0m%}'
 
   # ブランチマーク
   if ! git rev-parse 2> /dev/null; then
     # git 管理されていないディレクトリは何も返さない
     return
   fi
-    branch_name=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
-    st=`git status 2> /dev/null`
+
+  branch_name=`git rev-parse --abbrev-ref HEAD 2> /dev/null`
+  st=`git status 2> /dev/null`
+
   if [[ -n `echo "$st" | grep "^nothing to"` ]]; then
     # 全て commit されてクリーンな状態
-    branch_status="${color}${green}${branch}"
+    branch_status="${green}${branch}"
   elif [[ -n `echo "$st" | grep "^Untracked files"` ]]; then
     # git 管理されていないファイルがある状態
-    branch_status="${color}${red}${branch}?"
+    branch_status="${red}${branch}?"
   elif [[ -n `echo "$st" | grep "^Changes not staged for commit"` ]]; then
     # git add されていないファイルがある状態
-    branch_status="${color}${red}${branch}+"
+    branch_status="${red}${branch}+"
   elif [[ -n `echo "$st" | grep "^Changes to be committed"` ]]; then
     # git commit されていないファイルがある状態
-    branch_status="${color}${yellow}${branch}!"
+    branch_status="${yellow}${branch}!"
   elif [[ -n `echo "$st" | grep "^rebase in progress"` ]]; then
     # コンフリクトが起こった状態
-    echo "${color}${red}${branch}!(no branch)${reset}"
+    echo "${red}${branch}!(no branch)${reset}"
     return
   else
     # 上記以外の状態の場合
-    branch_status="${color}${blue}${branch}"
+    branch_status="${blue}${branch}"
   fi
 
   # ブランチ名を色付きで表示する
-  echo "${branch_status}$branch_name${reset}"
+  echo "${branch_status}${branch_name}${reset}"
 }
 
 # プロンプトが表示されるたびにプロンプト文字列を評価、置換する
 setopt prompt_subst
 
-# プロンプトの右側にメソッドの結果を表示させる
-RPROMPT='`rprompt-git-current-branch`'
+PROMPT=`prompt`
+RPROMPT='`git-current-branch`'
+
+# コマンドの実行ごとに改行
+function precmd() {
+  if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+    NEW_LINE_BEFORE_PROMPT=1
+  elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+    echo ""
+  fi
+}
