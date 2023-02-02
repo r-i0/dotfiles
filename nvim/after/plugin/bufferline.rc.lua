d6f96d3ce0891df59a18570f63052ab4c21fd751
riosudo <badbad7351@gmail.com>
2023-02-03 08:06:10 +0900

    fix bufferline

--- nvim/after/plugin/bufferline.rc.lua
+++ nvim/after/plugin/bufferline.rc.lua
@@ -3,11 +3,11 @@ if (not status) then return end
 
 bufferline.setup({
   options = {
-    mode = "tabs",
+    mode = "buffers",
     separator_style = 'slant',
     always_show_bufferline = true,
     show_buffer_close_icons = false,
-    show_close_icon = false,
+    show_close_icon = true,
     color_icons = true
   },
   highlights = {
